96e262705be38c37467d23ca4122a9614933b03c
elderking <test@example.com>
2019-10-10 20:01:11 +0800

    utils: use sha2 to replace rust-crypto

--- src/utils.rs
+++ src/utils.rs
@@ -1,57 +1,38 @@
 #![allow(non_snake_case)]
-#![allow(dead_code)]
 extern crate oath;
 extern crate rand;
-extern crate crypto;
+extern crate sha2;
 
 use std::time;
-use crypto::md5::Md5;
-use crypto::sha2::Sha256;
-use crypto::digest::Digest;
+use sha2::{Sha256, Digest};
 
 pub fn get_secs_now() -> u64 {
     let sys_time = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap();
     sys_time.as_secs()
 }
 
-pub fn md5_bytes(data:&str) -> [u8; 16] {
-    let mut buf  = [0;16];
-    let mut hasher = Md5::new();
-    hasher.input_str(data);
-    hasher.result(&mut buf);
-    buf
-}
-
-pub fn md5_bytes_from_bytes(data: &[u8]) -> [u8; 16] {
-    let mut buf  = [0;16];
-    let mut hasher = Md5::new();
-    hasher.input(data);
-    hasher.result(&mut buf);
-    buf
-}
-
-pub fn sha256_bytes(data:&str) -> [u8; 32] {
-    let mut buf  = [0;32];
+pub fn sha256_bytes(data:&str) -> [u8;32] {
+    let mut buf  = [0u8;32];
     let mut hasher = Sha256::new();
-    hasher.input_str(data);
-    hasher.result(&mut buf);
+    hasher.input(data.as_bytes());
+    buf.copy_from_slice(hasher.result().as_slice());
     buf
 }
 
-pub fn get_key_bytes(key:&str, otp:u32) -> [u8; 32] {
+pub fn get_key_bytes(key:&str, otp:u32) -> [u8;32] {
     let mut key = key.to_string();
     key.push_str(&format!("ThE=TuNnEL+SaLt-[];',/{}", otp));
     sha256_bytes(&key)
 }
 
-pub fn get_size_xor_bytes(key:&str, otp:u32) -> [u8; 32] {
+pub fn get_size_xor_bytes(key:&str, otp:u32) -> [u8;32] {
     let mut key = key.to_string();
     key.push_str(&format!("SaLT.fOr/SiZe+=Xor-{}", otp));
     sha256_bytes(&key)
 }
 
 pub fn get_random_bytes() -> Vec<u8> {
-    let mut length = rand::random::<usize>() % 20 + 8;
+    let mut length = rand::random::<usize>() % 20 + 12;
     let mut result = Vec::with_capacity(length);
     while length > 0  {
         result.push(rand::random::<u8>());
